e0ad69f6202eddbc0f60061fb471d1f41b45cb49
venkatswan1991 <venkatswan1991@gmail.com>
2026-03-17 14:46:26 +0530

    updated

--- 0-ec2-with-key/main.tf
+++ 0-ec2-with-key/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "sample_server" {
   ami                    = var.ami_id
-  instance_type          = var.instance_type
+  instancetype          = var.instance_type
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   user_data = file("workstation.sh")
 
@@ -14,6 +14,7 @@ resource "aws_instance" "sample_server" {
 
 resource "aws_key_pair" "my_key_pair" {
   key_name   = "venkat-key"
+  # Replace your ~/.ssh/venkat.pub key content below
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC9nw+W89MoPCuIXfeMYboxR+3ltb3spwXZiNRhWcP5KkZ5h5pBJRJdO+jL/ySV37ZcqzyYixbUzs/TvHOYsnj1MIew41JkZ0oqZYlbcy8rh74/ON2+r4YLMHA2Wd1CQ0lGifVwhYfjLRxwbL2z++1+obr1vUD2Wg3RuvMb9LRk70oiYVkt4Np4gvFhJTQVCKP9W7dv1dLCkP8sP8QfjM8i2/XwxhvGB52ur212Y6BKbue30xxfVmmRhEQH2qpMsIyIRGdEJ+0c/cTqbpvS4G+qNypNP/z0C+YXA6LLWVytV3aBUbe1Jde/9wqwuRsOizXyNg76CjjVgRPdtWHEstPys/lrGHNZa2+T051N+2CS2+QhzrqmTVViz2HKUE/BUKe703gbmgtQS5lyWQuvTaGWCxTxLYMwO8mHi4yk3afuwI4fguB4UGv6jEVjMFZ/jqUflAzczY6UmfZKMEq19AMja9L0H83UmGbODfpMKfiN6vM0BIeTdVdyEk4n3aFN30k= VENKATA REDDY@VENKAT"
 }
 
